d173f50c94d71d968e201a26f52d48fd667545f3
zygis <zygis.1415@outlook.es>
2020-03-09 12:41:42 +0100

    added icons and more styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import logo from "../../assets/netflix-2.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faGift, faBell } from '@fortawesome/free-solid-svg-icons'
+
 
 function Header() {
     return (
@@ -11,19 +14,28 @@ function Header() {
 
             <div className="header-menu">
                 <ul>
-                    <li><a className="active">Inicio</a></li>
-                    <li><a>Series TV</a></li>
-                    <li><a>Peliculas</a></li>
-                    <li><a>Mi lista</a></li>
+                    <li><a href={"/"} className="active">Inicio</a></li>
+                    <li><a href={"/"}>Series TV</a></li>
+                    <li><a href={"/"}>Peliculas</a></li>
+                    <li><a href={"/"}>Mas recientes</a></li>
+                    <li><a href={"/"}>Mi lista</a></li>
                 </ul>
             </div>
 
 
             <div className="header-options">
-                <div>Search</div>
-                <div>Ref</div>
-                <div>Notf</div>
-                <div>Profile</div>
+                <div><FontAwesomeIcon icon={faSearch} /></div>
+                <div><FontAwesomeIcon icon={faGift} /></div>
+                <div><FontAwesomeIcon icon={faBell} /></div>
+                <div className="account-menu">
+                    <div className="account-dropdown-menu">
+                        <span className="presentation">
+                            <img src={"https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABWyw3UeUMaPP3aLxzskg3Bn5htGHqQymxiEGTbfM91FZ0zJAPoEfYQdvHW7FV06FcdXhPC_4F7zNR7TFKLLv6yo.png?r=88c"} />
+                        </span>
+                        <span className="caret"></span>
+                    </div>
+
+                </div>
             </div>
 
         </header>
