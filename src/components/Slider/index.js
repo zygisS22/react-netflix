b7882ff891b551c7b4b7bd3ef3ecf78228a4ae1d
zygis <zygis.1415@outlook.es>
2020-03-09 22:09:46 +0100

    additional information about x movie

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SliderItem from "../SliderItem"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 function Slider() {
+
+
+
+
+
+    const [content, setContent] = useState(
+        [{
+            title: "test1",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+        {
+            title: "test2",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+        {
+            title: "test3",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+        {
+            title: "test4",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+        {
+            title: "test5",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+        {
+            title: "test6",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+        {
+            title: "test7",
+            img: "https://occ-0-2692-360.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTXnl2DtLEd4DBJ3JvTGk7QCJUuYESNwnnDHPx1RTM0eOn_xkcyVfp_jyrBc4_CRyNpAOPOYb7Xvjdv6kVL9s3nd5gU.jpg?r=31d"
+        },
+
+        ]
+    )
 
     return (
         <div className="sliderBox">
@@ -24,14 +61,9 @@ function Slider() {
 
                         <div className="slider-content">
 
-                            <SliderItem />
-                            <SliderItem />
-                            <SliderItem />
-                            <SliderItem />
-                            <SliderItem />
-                            <SliderItem />
-                            <SliderItem />
-                            <SliderItem />
+                            {content.map(item => {
+                                return <SliderItem />
+                            })}
 
                         </div>
 
@@ -42,6 +74,25 @@ function Slider() {
                     </span>
                 </div>
             </div>
+
+
+            <span className="jaw-bone-content">
+                <span className="jaw-bone-open-container">
+                    <div className="jaw-bone-container">
+                        <div className="background">
+
+                            <div className="jaw-bone-background">
+                                <div className="image-rotator"></div>
+                            </div>
+
+                            <div className="vignette"></div>
+                            <div className="nav-shadow"></div>
+                        </div>
+                        <div className="jaw-bone"></div>
+                        <button>close</button>
+                    </div>
+                </span>
+            </span>
 
         </div>
     );
