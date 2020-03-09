9532def4135facbb711910bf87d24d32b42dbae9
zygis <zygis.1415@outlook.es>
2020-03-09 12:21:24 +0100

    header simple structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import logo from "../../assets/netflix-2.svg"
 
 function Header() {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <header>
+            <div className="logo">
+                <img src={logo} alt={"logo"} />
+            </div>
+
+            <div className="header-menu">
+                <ul>
+                    <li><a className="active">Inicio</a></li>
+                    <li><a>Series TV</a></li>
+                    <li><a>Peliculas</a></li>
+                    <li><a>Mi lista</a></li>
+                </ul>
+            </div>
+
+
+            <div className="header-options">
+                <div>Search</div>
+                <div>Ref</div>
+                <div>Notf</div>
+                <div>Profile</div>
+            </div>
+
+        </header>
     );
 }
 
