0147f6bd61c5d8f7df38d940345b19c00a1491e9
zygis <zygis.1415@outlook.es>
2020-04-03 22:18:10 +0200

    footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,33 +7,14 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 function Footer() {
     return (
         <div className="footer">
+
+            <div className="copyright">
+                made by <span>zygisS22</span> using <span>React.js</span>
+            </div>
+
             <div className="social-links">
                 <a href={"https://github.com/zygisS22"}><FontAwesomeIcon icon={faGithub} /></a>
                 <a href={"https://www.linkedin.com/in/zygimantas-sniurevicius"}><FontAwesomeIcon icon={faLinkedin} /></a>
-            </div>
-
-            <ul className="footer-links">
-                <li className="footer-link-wrapper"><a href={"/"}>Audio y Subtitulos</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Descripcion de audio</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Centro de ayuda</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Tarjeta regalo</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Zona de prensa</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Inversores</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Empleo</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Terminos de uso</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Privacidad</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Avisos legales</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Preferencias de cookies</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Informacion corporativa</a></li>
-                <li className="footer-link-wrapper"><a href={"/"}>Contactanos</a></li>
-            </ul>
-
-            <div className="service-link">
-                <button>Codigo de servicio</button>
-            </div>
-
-            <div className="copyright">
-                © 1995-2020 Zygis, Inc
             </div>
         </div>
     );
