ea4ea47ecd61a33257a8b0afc5dde70328b96a52
zygis <zygis.1415@outlook.es>
2020-04-06 17:53:51 +0200

    spinning react

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faReact, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 function Footer() {
     return (
         <div className="footer">
 
-            <div className="copyright">
-                made by <span>zygisS22</span> using <span>React.js</span>
+            <div className="footer-content">
+                <span className="copyright">&copy; 2020 </span> made using <span className="react-icon"><FontAwesomeIcon icon={faReact} /></span> by
             </div>
 
             <div className="social-links">
                 <a href={"https://github.com/zygisS22"}><FontAwesomeIcon icon={faGithub} /></a>
-                <a href={"https://www.linkedin.com/in/zygimantas-sniurevicius"}><FontAwesomeIcon icon={faLinkedin} /></a>
             </div>
         </div>
     );
