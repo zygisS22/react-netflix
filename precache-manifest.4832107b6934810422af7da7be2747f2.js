f77dcec1ebbcb28e980bb316d2af07c35de4f40e
zygis <zygis.1415@outlook.es>
2020-04-07 02:20:04 +0200

    Updates

---
+++ precache-manifest.4832107b6934810422af7da7be2747f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91a9d25f00f261e680dd87e1a188e56",
+    "url": "/react-netflix/index.html"
+  },
+  {
+    "revision": "ff634332698cdeec99cc",
+    "url": "/react-netflix/static/css/main.04c358bf.chunk.css"
+  },
+  {
+    "revision": "95d5e355413caedf8282",
+    "url": "/react-netflix/static/js/2.0e143184.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/react-netflix/static/js/2.0e143184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff634332698cdeec99cc",
+    "url": "/react-netflix/static/js/main.98a52d7a.chunk.js"
+  },
+  {
+    "revision": "58b7a82f1f6ebb163a30",
+    "url": "/react-netflix/static/js/runtime-main.7826583a.js"
+  },
+  {
+    "revision": "b8ce8f65d789975ef0a762b4d092a9e0",
+    "url": "/react-netflix/static/media/netflix-2.b8ce8f65.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e1125646f0a7709556bcbf2b42d54d8.js"
+  "/react-netflix/precache-manifest.4832107b6934810422af7da7be2747f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-netflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
